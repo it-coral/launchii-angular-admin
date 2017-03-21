14eea568d43d2aafcff46801d5985be8f703300d
it-coral <coral1212@yahoo.com>
2017-03-21 08:22:58 +0000

    Changed env.js to be dynamic

--- env.js
+++ env.js
@@ -4,7 +4,7 @@
     window.__env = window.__env || {};
 
     // API url
-    window.__env.apiUrl = 'https://stageapi.launchii.com/v1';
+    window.__env.apiUrl = getApiUrl();
 
     // Base url
     //window.__env.baseUrl = '/';
@@ -12,4 +12,16 @@
     // Whether or not to enable debug mode
     // Setting this to false will disable console output
     window.__env.enableDebug = true;
+
+    function getApiUrl() {
+        var url = window.location.origin;
+
+        if (url.indexOf('launchii.dev')) { //Jellan's local
+            return 'https://stageapi.launchii.com/v1';
+        }
+
+        return url + '/v1';
+
+    }
+
 })(this);
