a8a30c34c06e423e505a34864f08a1131d4d20c6
it-coral <coral1212@yahoo.com>
2017-05-11 17:58:39 +0300

    Added metric boxes and graphs for basic GA report.

--- app/dashboard/controllers/dashboard.controller.js
+++ app/dashboard/controllers/dashboard.controller.js
@@ -4,10 +4,10 @@
     angular.module('app')
         .controller('DashboardController', DashboardController);
 
-    DashboardController.$inject = ['DashboardService', 'HelperService', '$window'];
+    DashboardController.$inject = ['DashboardService', 'HelperService', '$window', '$log'];
 
     /* @ngInject */
-    function DashboardController(DashboardService, HelperService, $window) {
+    function DashboardController(DashboardService, HelperService, $window, $log) {
         var vm = this;
 
         vm.summaryLoaded = false;
@@ -17,7 +17,13 @@
         vm.registeredVendorCount = 0;
         vm.runningDealCount = 0;
 
-        vm.response = {};
+        vm.summaryError = null;
+
+        vm.basicReport = null;
+        vm.basicChartData = null;
+        vm.firstLoadingFinished = false;
+
+        vm.analyticsError = null;
 
         activate();
 
@@ -25,6 +31,8 @@
             vm.page_title = "Dashboard";
 
             getSummary();
+
+            requestBasicReport();
         }
 
         function getSummary() {
@@ -39,14 +47,77 @@
                 vm.summaryLoaded = true;
 
             }).catch(function(err) {
-                vm.response['success'] = "alert-danger";
-                vm.response['alert'] = "Error!";
-                vm.response['msg'] = "Failed to pull dashboard info.";
-                vm.response['error_arr'] = err.data == null ? '' : err.data.errors;
-
-                HelperService.goToAnchor('msg-info');
+                vm.summaryError = err.data == null ? 'Something went wrong.' : err.data.errors;
             })
         }
-        
+
+        function requestBasicReport() {
+            DashboardService.getGAReportingData('basic').then(function(reports) {
+
+                if (reports.error) {
+                    vm.analyticsError = reports.error ? reports.error : 'Something went wrong.';
+                    return;
+                }
+
+                if (!reports.reports || !reports.reports[0].data.rows) {
+                    vm.basicReport = null;
+                    vm.firstLoadingFinished = true;
+                    return;
+                }
+
+                vm.basicReport = reports.reports[0];
+                vm.firstLoadingFinished = true;
+
+                // Build the chart data
+                vm.basicChartData = [];
+                for (var i = 0; i < vm.basicReport.data.rows.length; i ++) {
+                    var chartItem = {
+                        dimension: vm.basicReport.data.rows[i].dimensions[0],
+                        sessionsValue: vm.basicReport.data.rows[i].metrics[0].values[1],
+                        completions2Value: vm.basicReport.data.rows[i].metrics[0].values[5]
+                    }
+                    vm.basicChartData.push(chartItem);
+                }
+
+                // configure chart
+                var chart = new AmCharts.AmSerialChart();
+                chart.dataProvider = vm.basicChartData;
+                chart.categoryField = "dimension";
+                var legend = new AmCharts.AmLegend();
+                legend.useGraphSettings = true;
+                chart.addLegend(legend);
+
+                // configure category
+                var categoryAxis = chart.categoryAxis;
+                categoryAxis.labelRotation = 90;
+
+                // configure session graph
+                var graph1 = new AmCharts.AmGraph();
+                graph1.valueField = "sessionsValue";
+                graph1.type = "line";
+                graph1.bullet = "round";
+                graph1.lineColor = "blue";
+                graph1.balloonText = "[[category]]: <b>[[value]]</b>";
+                graph1.title = "Sessions";
+                chart.addGraph(graph1);
+
+                // configure shop now graph
+                var graph2 = new AmCharts.AmGraph();
+                graph2.valueField = "completions2Value";
+                graph2.type = "line";
+                graph2.bullet = "diamond";
+                graph2.lineColor = "red";
+                graph2.balloonText = "[[category]]: <b>[[value]]</b>";
+                graph2.title = "Shop - Now Clicks";
+                chart.addGraph(graph2);
+
+                chart.write("basic-report-chart");
+
+            }).catch(function(err) {
+                $log.log(err);
+                vm.analyticsError = 'Something went wrong.'
+            });
+        }
+
     }
 })();
